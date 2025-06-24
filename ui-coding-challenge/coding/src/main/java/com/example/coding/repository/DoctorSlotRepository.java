3dd524734c65544a92f82b1fe204067ab21623f0
Mallisavitha <2k21it34@kiot.ac.in>
2025-06-24 13:12:36 +0530

    ReactUiChallenge

--- ui-coding-challenge/coding/src/main/java/com/example/coding/repository/DoctorSlotRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.coding.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.coding.model.DoctorSlot;
-
-public interface DoctorSlotRepository extends JpaRepository<DoctorSlot, Integer> {
-
-}
